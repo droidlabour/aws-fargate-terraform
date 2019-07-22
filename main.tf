a17c51ded7139691d04c349680329cf3518ced02
Joy Prakash Sharma <droidlabour@gmail.com>
2019-07-22 15:30:59 +0530

    Add terraform for 4 ECS Fargate

--- main.tf
+++ main.tf
@@ -43,6 +43,20 @@ module "taskdef_2" {
 }
 
 
+module "taskdef_3" {
+  source       = "./modules/taskdef"
+  ecr_image    = var.ecr_3
+  taskdef_name = var.app_name_3
+}
+
+
+module "taskdef_4" {
+  source       = "./modules/taskdef"
+  ecr_image    = var.ecr_4
+  taskdef_name = var.app_name_4
+}
+
+
 module "app_1" {
   source               = "./modules/fargate"
   vpc_id               = module.vpc.vpc_id
@@ -74,6 +88,40 @@ module "app_2" {
   target_group_name    = var.app_name_2
   security_group_id    = module.security_group.fargate_security_group_id
   fargate_service_name = var.app_name_2
+}
+
+
+module "app_3" {
+  source               = "./modules/fargate"
+  vpc_id               = module.vpc.vpc_id
+  app_path             = var.app_path_3
+  listener_arn         = module.alb.listener_arn
+  subnet_1_id          = module.vpc.private_subnet_1_id
+  subnet_2_id          = module.vpc.private_subnet_2_id
+  container_name       = var.app_name_3
+  container_port       = 80
+  ecs_cluster_id       = "${aws_ecs_cluster.ecs_cluster.id}"
+  task_definition      = module.taskdef_3.arn
+  target_group_name    = var.app_name_3
+  security_group_id    = module.security_group.fargate_security_group_id
+  fargate_service_name = var.app_name_3
+}
+
+
+module "app_4" {
+  source               = "./modules/fargate"
+  vpc_id               = module.vpc.vpc_id
+  app_path             = var.app_path_4
+  listener_arn         = module.alb.listener_arn
+  subnet_1_id          = module.vpc.private_subnet_1_id
+  subnet_2_id          = module.vpc.private_subnet_2_id
+  container_name       = var.app_name_4
+  container_port       = 80
+  ecs_cluster_id       = "${aws_ecs_cluster.ecs_cluster.id}"
+  task_definition      = module.taskdef_4.arn
+  target_group_name    = var.app_name_4
+  security_group_id    = module.security_group.fargate_security_group_id
+  fargate_service_name = var.app_name_4
 }
 
 
--- variables.tf
+++ variables.tf
@@ -30,6 +30,14 @@ variable "app_name_2" {
   default = "thanos"
 }
 
+variable "app_name_3" {
+  default = "hawkeye"
+}
+
+variable "app_name_4" {
+  default = "tony"
+}
+
 variable "app_path_1" {
   default = "/v1*"
 }
@@ -38,10 +46,26 @@ variable "app_path_2" {
   default = "/v2*"
 }
 
+variable "app_path_3" {
+  default = "/v3*"
+}
+
+variable "app_path_4" {
+  default = "/v4*"
+}
+
 variable "ecr_1" {
   default = "142700881312.dkr.ecr.us-east-1.amazonaws.com/v1:latest"
 }
 
 variable "ecr_2" {
   default = "142700881312.dkr.ecr.us-east-1.amazonaws.com/v2:latest"
+}
+
+variable "ecr_3" {
+  default = "142700881312.dkr.ecr.us-east-1.amazonaws.com/v3:latest"
+}
+
+variable "ecr_4" {
+  default = "142700881312.dkr.ecr.us-east-1.amazonaws.com/v4:latest"
 }
